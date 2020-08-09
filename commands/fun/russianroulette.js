11dde765cbf8ce0380e1894c1856d1f9533e83b1
CelesteMagisteel <kamg1134@gmail.com>
2020-08-09 02:46:53 +0100

    Fixed mispluralization of bullet when using more than 1 bullet

--- commands/fun/russianroulette.js
+++ commands/fun/russianroulette.js
@@ -14,8 +14,8 @@ module.exports = {
     // Code to be executed when the command is run
     execute(message, args) {
         let rr_bullet = Math.floor(Math.random() * (6 - 1) + 1);
-        let rr_count = isNaN(args) ? 1 : args[0];
-        message.channel.send("You spin the cylinder of the revolver with " + rr_count + " bullet in it...");
+        let rr_count = isNaN(args[0]) ? 1 : args[0];
+        message.channel.send("You spin the cylinder of the revolver with " + rr_count + "" + (rr_count === 1 ? " bullet" : " bullets") + " in it...");
         setTimeout(function() {
             message.channel.send("...you place the muzzle against your head and pull the trigger...");
         }, 1000);
