b23cd4fd2e603dbe4ee92fe8ebede5dc2d3a463b
CelesteMagisteel <kamg1134@gmail.com>
2020-08-09 02:43:27 +0100

    Allowed for bullet count in russian rouletted

--- commands/fun/russianroulette.js
+++ commands/fun/russianroulette.js
@@ -4,23 +4,23 @@ module.exports = {
     // The Category of the command (Used for help)
     category: "fun",
     // Description of the command for the help menu
-    description: "",
+    description: "Test your luck at the old russian passtime",
     // Aliases that the command will also trigger
     aliases: ["rr", "russian", "roulette"],
     // Additional values that the command would require, to be added to help
-    usage: '',
+    usage: ' [bullets]',
     // If the command REQUIRES arguments, then this should be set to true
     args: false,
     // Code to be executed when the command is run
     execute(message, args) {
         let rr_bullet = Math.floor(Math.random() * (6 - 1) + 1);
-        let rr_count = 1;
-        message.channel.send("You spin the cylinder of the revolver with 1 bullet in it...");
+        let rr_count = isNaN(args) ? 1 : args[0];
+        message.channel.send("You spin the cylinder of the revolver with " + rr_count + " bullet in it...");
         setTimeout(function() {
             message.channel.send("...you place the muzzle against your head and pull the trigger...");
         }, 1000);
         setTimeout(function() {
-            if (rr_bullet === rr_count) {
+            if (rr_bullet <= rr_count) {
                 message.channel.send("...your brain gets splattered all over the wall.");
             } else {
                 message.channel.send("...you live to see another day.");
