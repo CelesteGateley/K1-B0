b9f916d8324fd34f825f1b96e8c8b76a091f129b
Celeste Gateley <celeste.gateley@gmail.com>
2021-02-13 19:43:47 +0000

    Fixed bug where shoot command could not check permissions of user

--- commands/fun/shoot.js
+++ commands/fun/shoot.js
@@ -19,17 +19,18 @@ module.exports = {
         let image_shot = appRoot + `/assets/images/shot/${randint(1, 10)}.gif`;
         let image_kms = appRoot + `/assets/images/suicide/${randint(1, 6)}.gif`;
         let image_geno = appRoot + `/assets/images/geno/${randint(1, 4)}.gif`;
-        let user = message.mentions.users.first() || message.author;
+        let rawUser = message.mentions.users.first() || message.author;
+        let user = message.guild.members.cache.get(rawUser.id);
 
         if (args[0] === "@everyone" || args[0] === "@here" || args[0] === "everyone") { user = "@everyone"; }
 
         if (message.author.id === user.id) {
             message.channel.send(`${message.author} commited suicide!`, { files: [image_kms] });
-        } else if ((user.id === "147590676391919616"  || user.permissions.has("ADMINISTRATOR")) && randint(1, 2) === 1) {
-            message.channel.send(`You attempted to shoot ${user}, but they dodged your bullet! Better luck next time, ${message.author}`, { files: [image_dodge] });
         } else if (user === "@everyone" || user === "everyone") {
             message.channel.send(`${message.author} decided to commit genocide!`, {files: [image_geno]});
-        } else if ((user.id === "147590676391919616") && randint(1, 3) === 1) {
+        } else if ((user.id === "147590676391919616"  || user.permissions.has("ADMINISTRATOR")) && randint(1, 2) === 1) {
+            message.channel.send(`You attempted to shoot ${user}, but they dodged your bullet! Better luck next time, ${message.author}`, { files: [image_dodge] });
+        }  else if ((user.id === "147590676391919616") && randint(1, 3) === 1) {
             message.reply(`Haha! You thought shooting me would be that easy?? You fool!!!`, { files: [image_dodge]});
         } else if (user !== message.author && (randint(1, 20) === 5 && (user.presence.status !== 'offline' && user.presence.status !== 'idle'))) {
             message.channel.send(`You attempted to shoot ${user}, but they dodged your bullet! Better luck next time, ${message.author}`, { files: [image_dodge] });
